8c7797394437a9843766148fd601c64f3b31a3bd
Egor <predsedatel228@yandex.ru>
2024-10-04 15:58:22 +0300

    add solution for read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFileSync, existsSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const filename = fileURLToPath(import.meta.url);
+  const fileToRead = join(dirname(filename), 'files', 'fileToRead.txt');
+  if (!existsSync(fileToRead)) {
+    throw new Error('FS operation failed');
+  } else {
+    const data = readFileSync(fileToRead, 'utf8');
+    console.log(data);
+  }
 };
 
 await read();
