d8730ecf2dff59852c42d81782b816fb773c5189
Egor <predsedatel228@yandex.ru>
2024-10-04 15:50:35 +0300

    add solution for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { promises as fs, existsSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const list = async () => {
-    // Write your code here 
+  const filename = fileURLToPath(import.meta.url);
+  const directory = join(dirname(filename), 'files');
+  if (!existsSync(directory)) {
+    throw new Error('FS operation failed');
+  } else {
+    const files = await fs.readdir(directory);
+    files.map(async (file) => {
+      console.log(file);
+    });
+  }
 };
 
 await list();
