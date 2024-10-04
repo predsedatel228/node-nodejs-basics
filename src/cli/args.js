fe92eee96a343e8e5fb7b9f9864f486b68fbaaa2
Egor <predsedatel228@yandex.ru>
2024-10-04 16:45:04 +0300

    add solution for args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    const answer = [];  
+    for (let i=2; i<=args.length-2; i+=2) {
+      answer.push(`${args[i].slice(2)} is ${args[i+1]}`)
+    }
+    console.log(answer.toString());
 };
 
 parseArgs();
