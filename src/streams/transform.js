8b662170420ccb672012a253f0efd2783d9c1007
Egor <predsedatel228@yandex.ru>
2024-10-05 16:21:59 +0300

    add solution for transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from 'node:stream'
+
 const transform = async () => {
-    // Write your code here 
-};
+  process.stdout.write('Please write text:\n (type Ctrl+C to exit)\n');
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, chunk.toString().split('').reverse().join(''))
+    }
+  })
+    process.stdin
+    .pipe(reverseStream)
+    .pipe(process.stdout);
+}
 
 await transform();
