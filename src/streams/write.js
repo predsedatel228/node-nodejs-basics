cc81c84ad1b5aeaaa1365405bfed31f65f376f8e
Egor <predsedatel228@yandex.ru>
2024-10-05 12:48:33 +0300

    fix solution for write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -5,7 +5,7 @@ import { dirname, join } from 'path';
 const write = async () => {
   const filename = fileURLToPath(import.meta.url);
   const fileToWrite = join(dirname(filename), 'files', 'fileToWrite.txt');
-  const input = readline.createInterface(process.stdin);
+//   const input = readline.createInterface(process.stdin);
   if (!existsSync(fileToWrite)) {
     throw new Error(`file does not exist`);
   } else {
