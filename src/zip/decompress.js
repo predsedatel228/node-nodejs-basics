2b77115496e62786e084880bf9096118f9e26152
Egor <predsedatel228@yandex.ru>
2024-10-05 17:06:40 +0300

    add solution for compress.js and decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import zlib from 'node:zlib'; 
+import { existsSync, createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+    const filename = fileURLToPath(import.meta.url);
+    const fileToWrite = join(
+      dirname(filename),
+      'files',
+      'fileToCompress.txt'
+    );
+    const fileToRead = join(
+        dirname(filename),
+        'files',
+        'archive.gz'
+      );
+    if (!existsSync(fileToRead)) {
+        throw new Error(`file does not exist`);
+      } else {
+        const readStream = createReadStream(fileToRead);
+        const writeStream = createWriteStream(fileToWrite)
+        const gzip = zlib.createUnzip();
+        readStream.pipe(gzip).pipe(writeStream);
+      }
 };
 
 await decompress();
